c2dc6814ea779fcf5ff566caf0fb1c7e3a6705c5
Matthew O <mo@pop-os.localdomain>
2020-09-23 19:40:38 -0700

    Added a loop for multiple projects

--- app.js
+++ app.js
@@ -17,12 +17,99 @@ fs.writeFile("index.html", generatePage(name, github), (err) => {
 });
 */
 
-inquirer
-  .prompt([
+// puts inquirer prompt in function and asks user for information
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name",
     },
-  ])
-  .then((answers) => console.log(answers));
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself",
+    },
+  ]);
+};
+
+const promptProject = (portfolioData) => {
+  // checks if array exists first before creating a new one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  // creates a clean line
+  console.log(`
+    ==================
+    Add a New Project
+    ==================
+    `);
+  // runs inquirer to gather info from user
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the name of your project?",
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Provide a description of the project (required)",
+        },
+        {
+          type: "checkbox",
+          name: "languages",
+          message:
+            "What did you build this project with? (Check all that apply)",
+          choices: [
+            "Javascript",
+            "HTML",
+            "CSS",
+            "ES6",
+            "jQuery",
+            "Bootstrap",
+            "Node",
+          ],
+        },
+        {
+          type: "input",
+          name: "link",
+          message: "Enter the Github link to your project (Required)",
+        },
+        {
+          type: "confirm",
+          name: "feature",
+          message: "Would you like to feature this project?",
+          default: false,
+        },
+        {
+          type: "confirm",
+          name: "confirmAddProject",
+          message: "Would you like to enter another project?",
+          default: false,
+        },
+      ])
+      // takes a promise from inquirer gathered data then pushes it to created array
+      .then((projectData) => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProjects) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      })
+  );
+};
+
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    console.log(portfolioData);
+  });
